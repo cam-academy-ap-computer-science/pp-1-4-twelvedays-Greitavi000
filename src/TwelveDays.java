2b3435aee32dd6bfa71424ef060deff26bd7324e
greitavi000 <greitavi000@CAM-LAB-16.bgsd.k12.wa.us>
2019-09-17 07:48:01 -0700

    TwelveDays.Java

--- src/TwelveDays.java
+++ src/TwelveDays.java
@@ -3,7 +3,48 @@ public class TwelveDays {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.print("On the first");
+		Christmas();
+		Tree();
+		System.out.print("On the second");
+		Christmas();
+		LastLines();
+		System.out.print("On the twelfth");
+		Christmas();
+		LastVirse();
+		LastLines();
 
 	}
-
+		
+	public static void Christmas() {
+		System.out.println(" day of Christmas,");
+		System.out.println("my true love sent to me");
+		
+	}
+   public static void Tree() {
+		System.out.println("a partridge in a pear tree.");
+		System.out.println(" ");
+   }
+   public static void Turtle() {
+		System.out.println("two turtle doves, and ");
+		
+		
+   }
+   public static void LastVirse() {
+   System.out.println("Twelve drummers drumming,");	
+	System.out.println("eleven pipers piping,");	
+	System.out.println("ten lords a-leaping,");	
+	System.out.println("nine ladies dancing,");	
+	System.out.println("eight maids a-milking");	
+	System.out.println("seven swans a-swimming, ");	
+	System.out.println("six geese a-laying,");	
+	System.out.println("five gold rings,");	
+	System.out.println("four calling birds,");	
+	System.out.println("three French hens, ");	
+   }
+   public static void LastLines() {
+   Turtle();
+   Tree();
+   }
 }
